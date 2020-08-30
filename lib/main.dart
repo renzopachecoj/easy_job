6214289fa9c78fcef2f311a7cf578f4cfd72c102
Renzo Pacheco <renzopachecoj@gmail.com>
2020-08-30 15:08:13 -0500

    add api_ref, readme, firebase config, font

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new EasyJobApp());
 
-class EasyJobApp extends StatefulWidget{
+class EasyJobApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState(){
-      return new EasyJobState();
-    }
-}
-
-class EasyJobState extends State<EasyJobApp>{
-  @override
-  Widget build(BuildContext context) {
-    return null;
+  State<StatefulWidget> createState() {
+    return new EasyJobState();
   }
 }
+
+class EasyJobState extends State<EasyJobApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EasyJob',
+      theme: ThemeData(
+        fontFamily: 'Rubik',
+        primaryColor: Colors.black,
+        accentColor: Colors.blueAccent,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EasyJob"),
+          actions: <Widget>[
+            // action button
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.white),
+              onPressed: () {
+              },
+            ),
+            // action button
+            IconButton(
+              icon: Icon(Icons.refresh, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Center(
+          child: Text("Jobs Here"),
+        ),
+      ),
+    );
+  }
+}
+
